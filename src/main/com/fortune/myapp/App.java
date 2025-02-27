9006adf07253c5f014bb41dece2bebd18b4167c1
vishakha <vishakhadhonde16@gmail.com>
2025-02-27 09:33:20 +0000

    cm-1

---
+++ src/main/com/fortune/myapp/App.java
@@ -0,0 +1,19 @@
+package com.fortune.myapp;
+
+/**
+ * Hello world!
+ */
+public class App {
+
+    private static final String MESSAGE = "Hello World!";
+
+    public App() {}
+
+    public static void main(String[] args) {
+        System.out.println(MESSAGE);
+    }
+
+    public String getMessage() {
+        return MESSAGE;
+    }
+}
